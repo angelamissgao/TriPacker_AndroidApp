9ada6a2674b08d96a2fd764fc6e7489b81687c93
Angela Gao <angelagao@AngelatekiMacBook-Pro.local>
2016-04-14 13:36:53 -0700

    add controller

---
+++ app/src/main/java/com/example/tripacker/tripacker/controller/ActivityComponent.java
@@ -0,0 +1,10 @@
+package com.example.tripacker.tripacker.controller;
+
+import android.app.Activity;
+
+/**
+ * Created by angelagao on 4/14/16.
+ */
+public interface ActivityComponent {
+    Activity activity();
+}
---
+++ app/src/main/java/com/example/tripacker/tripacker/controller/SigninUser.java
@@ -0,0 +1,8 @@
+package com.example.tripacker.tripacker.controller;
+
+/**
+ * Created by angelagao on 4/14/16.
+ */
+public class SigninUser {
+
+}
