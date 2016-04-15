009bd9d728e3b87e6bdcda9999f00f88075aeec7
Eileen Wei <eileenwei0105@gmail.com>
2016-04-14 19:07:25 -0700

    Adding javax notation

---
+++ app/src/main/java/com/example/tripacker/tripacker/entity/mapper/TripEntityJsonMapper.java
@@ -0,0 +1,7 @@
+package com.example.tripacker.tripacker.entity.mapper;
+
+/**
+ * Created by EILEENWEI on 4/14/16.
+ */
+public class TripEntityJsonMapper {
+}
---
+++ app/src/main/java/com/example/tripacker/tripacker/entity/mapper/UserEntityJsonMapper.java
@@ -0,0 +1,63 @@
+package com.example.tripacker.tripacker.entity.mapper;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
+import com.google.gson.reflect.TypeToken;
+import java.lang.reflect.Type;
+import java.util.List;
+import javax.inject.Inject;
+
+import com.example.tripacker.tripacker.entities.UserEntity;
+
+/**
+ * Class used to transform from Strings representing json to valid objects.
+ */
+public class UserEntityJsonMapper {
+
+    private final Gson gson;
+
+    @Inject
+    public UserEntityJsonMapper() {
+        this.gson = new Gson();
+    }
+
+    /**
+     * Transform from valid json string to {@link UserEntity}.
+     *
+     * @param userJsonResponse A json representing a user profile.
+     * @return {@link UserEntity}.
+     * @throws com.google.gson.JsonSyntaxException if the json string is not a valid json structure.
+     */
+    public UserEntity transformUserEntity(String userJsonResponse) throws JsonSyntaxException {
+        try {
+            Type userEntityType = new TypeToken<UserEntity>() {}.getType();
+            UserEntity userEntity = this.gson.fromJson(userJsonResponse, userEntityType);
+
+            return userEntity;
+        } catch (JsonSyntaxException jsonException) {
+            throw jsonException;
+        }
+    }
+
+    /**
+     * Transform from valid json string to List of {@link UserEntity}.
+     *
+     * @param userListJsonResponse A json representing a collection of users.
+     * @return List of {@link UserEntity}.
+     * @throws com.google.gson.JsonSyntaxException if the json string is not a valid json structure.
+     */
+    public List<UserEntity> transformUserEntityCollection(String userListJsonResponse)
+            throws JsonSyntaxException {
+
+        List<UserEntity> userEntityCollection;
+        try {
+            Type listOfUserEntityType = new TypeToken<List<UserEntity>>() {}.getType();
+            userEntityCollection = this.gson.fromJson(userListJsonResponse, listOfUserEntityType);
+
+            return userEntityCollection;
+        } catch (JsonSyntaxException jsonException) {
+            throw jsonException;
+        }
+    }
+}
+
