9e8f423404741df68020e0d7f3432f6bc68b3973
Angela Gao <angelagao@AngelatekiMBP.lan>
2016-04-29 00:34:38 -0700

    custom grid view

--- app/src/main/java/com/example/tripacker/tripacker/view/fragment/SpotFragment.java
+++ app/src/main/java/com/example/tripacker/tripacker/view/fragment/SpotFragment.java
@@ -48,9 +48,18 @@ public class SpotFragment extends Fragment implements AsyncCaller, SpotListView{
     private Context thiscontext;
     public static final String ARG_PAGE = "ARG_PAGE";
     GridView gridView;
+
     @Inject
     SpotListPresenter spotListPresenter;
     ArrayList<SpotEntity> arrayOfSpots = new ArrayList<>();
+
+    private Integer[] imagesource = {
+            R.drawable.paris,
+            R.drawable.thai_temple,
+            R.drawable.new_zealand,
+            R.drawable.sf_night,
+            R.drawable.sf_museum
+    };
 
 
     public static SpotFragment newInstance() {
@@ -166,6 +175,12 @@ public class SpotFragment extends Fragment implements AsyncCaller, SpotListView{
                 spoti.put("spotId", spotId);
 
                 SpotEntity spotEntity = new SpotEntity(spoti);
+                //get image
+                int postion = (int)(Math.random() * imagesource.length);
+                int img_main = imagesource[postion];
+                spotEntity.setImage_source_local(img_main);
+
+                //add Spot Entity to array
                 arrayOfSpots.add(spotEntity);
 
             }
